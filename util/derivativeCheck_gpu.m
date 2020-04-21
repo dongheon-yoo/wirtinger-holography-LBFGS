637d4cd318dcdc825dd0dc3a1a3efb47c075e121
Dongheon Yoo <39788884+dongheon-yoo@users.noreply.github.com>
2020-04-21 20:16:25 +0900

    Update derivativeCheck_gpu.m

--- util/derivativeCheck_gpu.m
+++ util/derivativeCheck_gpu.m
@@ -1,46 +1,29 @@
 function [user_grad, num_grad, diff] = derivativeCheck_gpu(phase_vec, image, params)
-%% Derivative check for variables
-[~, ugrads] = loss_and_gradients_2D_optim(initVariables, params);
+%% Derivative check for gradient-based optimization
+[~, ugrads] = loss_and_gradients(phase_vec, image, params);
 
 % Perturbation
-mu = 1e-6;
-n_delta_pert = 10;
-user_grad = convertGPU(zeros(6 + n_delta_pert, 1)); user_grad(1 : 6) = ugrads(1 : 6); 
-num_grad = convertGPU(zeros(6 + n_delta_pert, 1)); 
-for i = 1 : 6
-    varCopy = initVariables;
-    var = varCopy(i);
+mu = 2e-6 * norm(phase_vec);
+n_pert = 10;
+user_grad = convertGPU(zeros(n_pert, 1)); 
+num_grad = convertGPU(zeros(n_pert, 1)); 
+
+for i = 1 : n_pert
+    idx = randi(length(phase_vec));
+    varCopy = phase_vec;
+    var = varCopy(idx);
     var = var + mu;
-    varCopy(i) = var;
-    [loss1, ~] = loss_and_gradients_2D_optim(varCopy, params);
+    varCopy(idx) = var;
+    [loss1, ~] = loss_and_gradients(varCopy, image, params);
     
-    varCopy = initVariables;
-    var = varCopy(i);
+    varCopy = phase_vec;
+    var = varCopy(idx);
     var = var - mu;
-    varCopy(i) = var;
-    [loss2, ~] = loss_and_gradients_2D_optim(varCopy, params);
-    
+    varCopy(idx) = var;
+    [loss2, ~] = loss_and_gradients(varCopy, image, params);
     ngrad = (loss1 - loss2) ./ (2 * mu);
     num_grad(i) = ngrad;
-end
-
-n_delta = length(initVariables(7 : end));
-for i = 1 : n_delta_pert
-    idx = randi(n_delta);
-    varCopy = initVariables;
-    var = varCopy(6 + idx);
-    var = var + mu;
-    varCopy(6 + idx) = var;
-    [loss1, ~] = loss_and_gradients_2D_optim(varCopy, params);
-    
-    varCopy = initVariables;
-    var = varCopy(6 + idx);
-    var = var - mu;
-    varCopy(6 + idx) = var;
-    [loss2, ~] = loss_and_gradients_2D_optim(varCopy, params);
-    ngrad = (loss1 - loss2) ./ (2 * mu);
-    num_grad(6 + i) = ngrad;
-    user_grad(6 + i) = ugrads(6 + idx);
+    user_grad(i) = ugrads(idx);
 end
 
 numerator = sqrt(sum((num_grad - user_grad) .^ 2, 'all'));
